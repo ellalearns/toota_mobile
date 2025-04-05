5c31ae9516451f5ebce4adef2bbcf2d3cf76e716
ellalearns <ellalearnscopy@gmail.com>
2025-04-05 22:04:52 +0100

    add role options ui

--- app/(onboarding)/role.tsx
+++ app/(onboarding)/role.tsx
@@ -1,27 +1,97 @@
 import React from "react";
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
+import images from "@/constants/images";
 
 const Role = () => {
-    return(
+    return (
         <View
-        style={{
-            flex: 1,
-            flexDirection: "column",
-            justifyContent: "center",
-        }}>
-            <View
             style={{
-                backgroundColor: "black",
-                flex: .6,
-                margin: 20,
-                borderRadius: 25,
-                elevation: 20,
-            }}></View>
+                flex: 1,
+                flexDirection: "column",
+                justifyContent: "center",
+                backgroundColor: "white"
+            }}>
             <View
-            style={{
-                backgroundColor: "green",
-                flex: .4
-            }}></View>
+                style={{
+                    // backgroundColor: "green",
+                    flex: .2
+                }}>
+
+            </View>
+            <View
+                style={{
+                    backgroundColor: "white",
+                    flex: .5,
+                    flexDirection: "column",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    margin: 20,
+                    elevation: 5,
+                    marginTop: 20,
+                    borderRadius: 30
+                }}>
+                {/* <View
+                style={{
+                    backgroundColor: "fef5e8",
+                    // justifyContent: "space-around",
+                    height: 20,
+                    // flex: 1
+                }}></View> */}
+
+                {/* <View
+                style={{
+                    backgroundColor: "fef5e8",
+                    // justifyContent: "space-around",
+                    // height: "20%",
+                    // flex: 1
+                }}></View> */}
+
+                <Text
+                    style={{
+                        fontSize: 40
+                    }}>Choose your role</Text>
+
+                <View
+                    style={{
+                        height: "30%",
+                        width: "80%",
+                        backgroundColor: "#fef5e8",
+                        borderRadius: 20,
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "space-evenly"
+                    }}>
+                        <Image source={images.findDriver} />
+                        <Text 
+                        style={{
+                            fontSize: 15,
+                            color: "#6b6357"
+                        }}>Find a trip</Text>
+                </View>
+                <View
+                    style={{
+                        height: "30%",
+                        width: "80%",
+                        backgroundColor: "#fef5e8",
+                        borderRadius: 20,
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "space-evenly"
+                    }}>
+                        <Image source={images.findTrip} />
+                        <Text 
+                        style={{
+                            fontSize: 15,
+                            color: "#6b6357"
+                        }}>Find a driver</Text>
+                </View>
+
+            </View>
+            <View
+                style={{
+                    backgroundColor: "green",
+                    flex: .3
+                }}></View>
         </View>
     )
 }
