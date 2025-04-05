3ab6f889193b245fdf7d2b35a7013df480690767
ellalearns <ellalearnscopy@gmail.com>
2025-04-05 19:45:01 +0100

    create colored stripes on splash screen

--- app/splash_screen.tsx
+++ app/splash_screen.tsx
@@ -1,16 +1,53 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Dimensions, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient"
 
 const SplashScreen = () => {
 
+    const { width, height } = Dimensions.get("screen")
+    const stripeHeight = height / 3
+
     return (
-        <View>
-            <Text>I was returned instead</Text>
+        <View
+            style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+                // backgroundColor: "#f99e1a"
+            }}>
+
+            <View
+                style={{
+                    backgroundColor: "#223311",
+                    position: "absolute",
+                    top: "25%",
+                    height: height / 5,
+                    width: width * 2,
+                    left: -70,
+                    right: 0,
+                    transform: [{
+                        rotate: "22deg"
+                    }]
+                }}></View>
+
+            <View
+                style={{
+                    backgroundColor: "#223311",
+                    position: "absolute",
+                    top: "70%",
+                    height: height / 5,
+                    width: width * 2,
+                    left: -70,
+                    right: 0,
+                    transform: [{
+                        rotate: "22deg",
+                        
+                    }]
+                }}></View>
+
         </View>
     )
 
 }
 
 export default SplashScreen
-
