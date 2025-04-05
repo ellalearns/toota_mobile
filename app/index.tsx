3815e1abfb131ecd96b0167c575b2620afb04569
ellalearns <ellalearnscopy@gmail.com>
2025-04-05 18:35:41 +0100

    set up splash screen component

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  const [splashScreen, setSplashScreen] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSplashScreen(false)
+    }, 2000)
+  });
+
   return (
-    <View
+    
+    (splashScreen) ? <View>
+      <Text>I was returned instead</Text>
+    </View> : <View
       style={{
         flex: 1,
         justifyContent: "center",
