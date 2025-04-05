cf3000166e3b27eee6b3b7b1f41d10da9bb4bd05
ellalearns <ellalearnscopy@gmail.com>
2025-04-05 21:27:24 +0100

    create two views on first onboarding screen

--- app/(onboarding)/role.tsx
+++ app/(onboarding)/role.tsx
@@ -3,7 +3,26 @@ import { View } from "react-native";
 
 const Role = () => {
     return(
-        <View></View>
+        <View
+        style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+        }}>
+            <View
+            style={{
+                backgroundColor: "black",
+                flex: .6,
+                margin: 20,
+                borderRadius: 25,
+                elevation: 20,
+            }}></View>
+            <View
+            style={{
+                backgroundColor: "green",
+                flex: .4
+            }}></View>
+        </View>
     )
 }
 
