84733ce72a12c7fc2507eef0abc0240278ae4ed5
ellalearns <ellalearnscopy@gmail.com>
2025-04-13 14:45:18 +0100

    make header icon

--- styles/styles_PageIcon.tsx
+++ styles/styles_PageIcon.tsx
@@ -1,26 +1,16 @@
 import { StyleSheet } from "react-native";
 
 const styles_PageIcon = StyleSheet.create({
-    main: {
-        height: 100,
-        width: 100,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff9f1",
-        borderRadius: 100
+    container: {
+        flex: 1
     },
-
-    second_main: {
-        height: 70,
-        width: 70,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fefaf3"
-    },
-
     image: {
         height: 50,
-        width: 50
+        width: 50,
+        position: "absolute",
+        top: 25,
+        left: 25,
+        right: 0
     }
 })
 
