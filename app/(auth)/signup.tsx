43923c5e0e76d941482cb184c065c2993d0a8851
ellalearns <ellalearnscopy@gmail.com>
2025-04-13 14:56:07 +0100

    complete header content

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,4 +1,6 @@
 import PageIcon from "@/components/PageIcon";
+import copy from "@/constants/texts";
+import styles_SignUp from "@/styles/styles_SignUp";
 import React from "react";
 import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
@@ -7,8 +9,17 @@ const SignUp = () => {
 
     return (
         <SafeAreaProvider>
-            <SafeAreaView>
+            <SafeAreaView style={styles_SignUp.main}>
+                <View style={styles_SignUp.headerContent}>
                 <PageIcon />
+                <Text style={styles_SignUp.signup1}>{copy.signup1}</Text>
+                <Text style={styles_SignUp.signup2}>{copy.signup2}</Text>
+                </View>
+
+
+                <View></View>
+                <View></View>
+
             </SafeAreaView>
         </SafeAreaProvider>
     )
--- styles/styles_SignUp.tsx
+++ styles/styles_SignUp.tsx
@@ -3,6 +3,21 @@ import { StyleSheet } from "react-native";
 const styles_SignUp = StyleSheet.create({
     main: {
         flex: 1,
+        padding: 16,
+        backgroundColor: "#ffffff",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    headerContent: {},
+    signup1: {
+        textAlign: "center",
+        color: "#1f1200",
+        fontSize: 24
+    },
+    signup2: {
+        textAlign: "center",
+        color: "#6b6357",
+        fontSize: 16
     }
 })
 
