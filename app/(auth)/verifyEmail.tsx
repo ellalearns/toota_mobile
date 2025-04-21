9a09b3a416b66d09126c853801bb2ed23349cabc
ellalearns <ellalearnscopy@gmail.com>
2025-04-21 15:10:04 +0100

    complete ui for otp page

--- app/(auth)/verifyEmail.tsx
+++ app/(auth)/verifyEmail.tsx
@@ -1,16 +1,36 @@
+import BackButton from "@/components/BackButton";
+import MainButton from "@/components/MainButton";
 import NumberFrame from "@/components/NumberFrame";
 import OTPFrame from "@/components/OTPFrame";
+import OutlineButton from "@/components/OutlineButton";
+import ViewBreak from "@/components/ViewBreak";
 import Styles_VerifyEmail from "@/styles/styles_VerifyEmail";
 import React from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
 import { SafeAreaFrameContext, SafeAreaProvider } from "react-native-safe-area-context";
 
 const VerifyEmail = () => {
     return (
         <SafeAreaProvider>
             <SafeAreaView style={Styles_VerifyEmail.main}>
-                <OTPFrame />
-                <NumberFrame />
+                <View style={Styles_VerifyEmail.default_padding}>
+                    <BackButton onPress={() => { }} />
+                </View>
+                <View style={Styles_VerifyEmail.default_padding}>
+                    <Text  style={Styles_VerifyEmail.header_text}>OTP Verification</Text>
+                    <Text style={Styles_VerifyEmail.body_text}>To complete your sign-up, please enter the OTP sent to your email  
+                        <Text style={Styles_VerifyEmail.email}> urmail@gmail.com</Text>
+                        </Text>
+                </View>
+                <ViewBreak />
+                <View style={Styles_VerifyEmail.default_padding}>
+                    <OTPFrame />
+                    <NumberFrame />
+                </View>
+                <View style={Styles_VerifyEmail.default_padding}>
+                    <MainButton text="Continue" pressFun={() => { }} isEmpty />
+                    <OutlineButton text="Resend code" pressFun={() => { }} />
+                </View>
             </SafeAreaView>
         </SafeAreaProvider>
     )
--- styles/styles_VerifyEmail.tsx
+++ styles/styles_VerifyEmail.tsx
@@ -4,10 +4,25 @@ const Styles_VerifyEmail = StyleSheet.create({
     main: {
         backgroundColor: "#ffffff",
         paddingTop: 24,
+        paddingBottom: 20,
+        flex: 1,
+        justifyContent: "space-around"
+    },
+    default_padding: {
         paddingLeft: 20,
         paddingRight: 20,
-        paddingBottom: 20,
-        flex: 1
+    },
+    header_text: {
+        fontSize: 24,
+        fontWeight: 700
+    },
+    body_text: {
+        fontSize: 16,
+        fontWeight: 400,
+        paddingTop: 8
+    },
+    email: {
+        fontWeight: 700
     }
 })
 
