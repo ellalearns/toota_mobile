12ff0093ba70c590555e3444811467fe02499e09
steve2700 <stevnyaru@github.com>
2025-06-09 20:00:46 +0200

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function RootLayout() {
-  return <Stack 
-  screenOptions={{ headerShown: false }}/>;
+export default function Layout() {
+  return (
+    <SafeAreaProvider>
+      <Slot />
+    </SafeAreaProvider>
+  );
 }
+
--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,26 @@
 import { useState, useEffect } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// Screens
-import DashboardScreen from "./dashboard_user/DashboardScreen";
-import RouteScreen from "./dashboard_user/routescreen";
+import { Text, View } from "react-native";
 import SplashScreen from "./splash_screen";
-
-const Stack = createNativeStackNavigator();
+import Role from "./(onboarding)/role";
+import KycIntro from "./(onboarding)/kycIntro";
+import Kyc from "./(onboarding)/kyc";
 
 export default function Index() {
-  const [splashScreen, setSplashScreen] = useState(true);
+
+  const [splashScreen, setSplashScreen] = useState(true)
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setSplashScreen(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (splashScreen) {
-    return <SplashScreen />;
-  }
+    setTimeout(() => {
+      setSplashScreen(false)
+    }, 2000)
+  });
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Dashboard" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        <Stack.Screen name="RouteScreen" component={RouteScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    (splashScreen) ? <SplashScreen /> : <Role />
+    // <View>
+    //   <Text>Working on Local Phone</Text>
+    // </View>
+    // <KycIntro />
+    // <Kyc />
   );
 }
-
